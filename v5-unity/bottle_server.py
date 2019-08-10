a7c2c29014d079a128cf2c066ca6ee7ac8552b60
cmattey <cmattey@dons.usfca.edu>
2019-08-09 18:07:16 -0700

    local port changed to 5000

--- v5-unity/bottle_server.py
+++ v5-unity/bottle_server.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
     if os.environ.get('APP_LOCATION')=='heroku':
         run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)), reloader=True)
     else:
-        run(host='localhost', port=8003, reloader=True)
+        run(host='localhost', port=5000, reloader=True)
