6db837ec20899e54790f8f568964bdbee36d3282
cmattey <cmattey@dons.usfca.edu>
2019-08-09 17:16:32 -0700

    Added server config vars for remote server on heroku

--- v5-unity/bottle_server.py
+++ v5-unity/bottle_server.py
@@ -62,4 +62,7 @@ def get_py_exec():
 
 
 if __name__ == "__main__":
-    run(host='localhost', port=8003, reloader=True)
+    if os.environ.get('APP_LOCATION')=='heroku':
+        run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)), reloader=True)
+    else:
+        run(host='localhost', port=8003, reloader=True)
