2d993980776e99f3f3df480d9e436bebf0b8e2a2
cmattey <cmattey@dons.usfca.edu>
2019-09-18 20:34:58 -0700

    Updated https condition

--- v5-unity/js/opt-frontend-common.ts
+++ v5-unity/js/opt-frontend-common.ts
@@ -442,9 +442,9 @@ export abstract class AbstractBaseFrontend {
 
         // 2018-08-19: this is an uncommon use case (only used for https iframe embedding)
         if (jsonp_endpoint.indexOf('https:') == 0) {
-            this.setFronendError(["Error: https execution of non-Python code is not currently supported. [#nonPythonHttps]"]);
-            this.doneExecutingCode();
-            return;
+            // this.setFronendError(["Error: https execution of non-Python code is not currently supported. [#nonPythonHttps]"]);
+            // this.doneExecutingCode();
+            // return;
         }
 
         var retryOnBackupServer = () => {
